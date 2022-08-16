f4c84f08c763ba01e569018f0701ee5fe456802d
Jose Beja <beja.jose@gmail.com>
2022-08-16 12:03:33 -0300

    agregamos un DOM a la pagina Index

---
+++ js/appIndex.js
@@ -0,0 +1,10 @@
+
+ let nombre = prompt("Ingresa tu nombre");
+
+//creamos dom para nombre de usuario.
+// document.querySelector("h1")
+// console.log(document.getElementById("titulo"))
+
+const h1 = document.querySelector("h1");
+    h1.textContent = "Bienvenido" + " " + nombre;
+
